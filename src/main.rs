dd9dfc34d99b0e54bba578508ed994f821f59fc3
Thomas Walker <codemonkeweb@proton.me>
2025-01-04 23:54:38 +0000

    Updated logging functionality so that we can check the output of rfind for testing

--- src/main.rs
+++ src/main.rs
@@ -55,34 +55,65 @@ fn main() {
     println!("Starting_path: {}", starting_path);
     println!("Name: {}", name);
 
-    search_working_directory(Path::new(starting_path), name);
+    search_working_directory(Path::new(starting_path), name, StandardLogger::new());
 }
 
 enum LogLine {
-    Normal(String),
-    Warning(String),
-    Error(String)
+    StdOut(String),
+    StdErr(String)
 }
 
-struct Logger {
-    lines: Vec::<LogLine>,
-    log_immediately: bool
+trait Logger {
+    fn log(&mut self, line_to_log: LogLine);
 }
 
-impl Logger {
-    fn new() -> Logger {
-        Logger {
-            lines: Vec::new(),
-            log_immediately: true
+struct TestLogger {
+    log: Vec<LogLine>
+}
+
+impl TestLogger {
+    fn new() -> TestLogger {
+        TestLogger {
+            log: Vec::new()
+        }
+    }
+
+    fn get_logs(&self) -> &Vec<LogLine> {
+        &self.log
+    }
+}
+
+impl Logger for TestLogger {
+    fn log(&mut self, line_to_log: LogLine) {
+        self.log.push(line_to_log);
+    }
+}
+
+
+struct StandardLogger { }
+
+impl StandardLogger {
+    fn new() -> StandardLogger {
+        StandardLogger {
+        }
+    }
+}
+
+impl Logger for StandardLogger {
+    fn log(&mut self, line_to_log: LogLine) {
+        _ = match line_to_log {
+            LogLine::StdOut(line) => println!("{}", line),
+            LogLine::StdErr(line) => eprintln!("{}", line),
         }
     }
 }
 
 
-fn search_working_directory(working_directory: &Path, name: &str/*, logger: &mut Logger*/) {
+fn search_working_directory(working_directory: &Path, name: &str, logger: &mut Logger) {
 // fn search_working_directory(working_directory: &str, name: &str) {
     // Check the contents of the current working directory.
-    println!("{:#?}", working_directory);
+
+    // println!("{:#?}", working_directory);
 
     let read_dir = match fs::read_dir(working_directory) {
         Ok(res) => {
@@ -102,13 +133,10 @@ fn search_working_directory(working_directory: &Path, name: &str/*, logger: &mut
     for ele in read_dir.into_iter() {
         let ele = ele.unwrap();
         let file_name = ele.file_name();
-        println!("In loop! file_name: {}", file_name.to_str().unwrap());
-        let metadata = ele.metadata();
 
         
-        println!("still alive!");
         if file_name == name {
-            println!("{:#?}/{}", working_directory, name);
+            logger.log(format!("{}/{}", working_directory.to_str().unwrap(), name));
             continue;
         }
         let file_type = ele.file_type().unwrap();
@@ -124,7 +152,7 @@ fn search_working_directory(working_directory: &Path, name: &str/*, logger: &mut
             let file_name: &str = file_name.to_str().unwrap();
             let working_directory = working_directory.join(file_name);
             let working_directory = working_directory.as_path();
-            search_working_directory(working_directory, name);
+            search_working_directory(working_directory, name, logger);
 
         }
     }
@@ -161,20 +189,52 @@ mod tests {
     fn find_file_in_same_directory_3() -> Result<(), Box<dyn std::error::Error>> {
         // Arrange
         assert_eq!(tempfile::env::temp_dir(), std::env::temp_dir());
+        TestLogger::new();
 
         // Create a file inside of `env::temp_dir()`.
         let file = NamedTempFile::new()?;
         let file_name = file.path().file_name().unwrap().to_str().unwrap();
-        search_working_directory(tempfile::env::temp_dir().as_path(), file_name);
+        search_working_directory(tempfile::env::temp_dir().as_path(), file_name, TestLogger::new());
         Ok(())
     }
 
     #[test]
-    fn find_file_in_same_directory_2() -> Result<(), Box<dyn std::error::Error>> {
+    fn find_file_in_child_directory() -> Result<(), Box<dyn std::error::Error>> {
         // Arrange
         assert_eq!(tempfile::env::temp_dir(), std::env::temp_dir());
+        TestLogger::new();
+        
+        // Create a directory inside of `env::temp_dir()`
+        let directory = TempDir::new()?;
+        let file_path = directory.path().join("find_file_in_child_directory.txt");
+        let tmp_file = File::create(file_path)?;
 
-        // create new temp file
+        // Act
+        search_working_directory(tempfile::env::temp_dir().as_path(), "find_file_in_child_directory.txt");
+
+        drop(tmp_file);
+        directory.close()?;
+        Ok(())        
+    }
+
+    #[test]
+    fn find_file_in_child_child_directory() -> Result<(), Box<dyn std::error::Error>> {
+        // Arrange
+        assert_eq!(tempfile::env::temp_dir(), std::env::temp_dir());
+        TestLogger::new();
+
+        // let directory = TempDir::new()?;
+        let directory = Builder::new().prefix("find_file_in_child_child_directory").tempdir().unwrap();
+        let temp_dir_child = Builder::new().prefix("find_file_in_child_child_directory").tempdir_in(directory.path()).unwrap();
+        let tmp_file_path = temp_dir_child.path().join("find_file_in_child_child_directory.txt");
+        let tmp_file = File::create(tmp_file_path);
+        
+        // Act
+        search_working_directory(tempfile::env::temp_dir().as_path(), "find_file_in_child_child_directory.txt");
+
+        drop(tmp_file);
+        temp_dir_child.close()?;
+        directory.close()?;
         Ok(())
     }
 
@@ -194,7 +254,7 @@ mod tests {
         // println!("file name: {:#?}", file_name);
 
         // Act
-        search_working_directory(working_directory.as_path(), "find_file_in_same_directory");
+        search_working_directory(working_directory.as_path(), "find_file_in_same_directory", TestLogger::new());
 
 
         println!("{:#?}", tempfile::env::temp_dir());
