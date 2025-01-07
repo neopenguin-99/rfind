dd286e730c631614a2eec872574a5bd014420a18
Thomas Walker <codemonkeweb@proton.me>
2025-01-07 23:53:53 +0000

    Added type test, in the hopes that in the future, multiple tests can be combined with expressions with the help of peg

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+use std::intrinsics::unreachable;
 use std::io;
 use std::mem::{discriminant, Discriminant};
 use std::io::{Write, Read, Seek, SeekFrom};
-use std::fs;
+use std::fs::{self, FileType};
+use std::os::unix::fs::FileTypeExt;
 use std::path::Path;
 use std::process::exit;
 use std::env;
 use clap::{value_parser, Arg, ArgAction, ArgMatches, Command, ValueEnum};
 use std::fs::File;
 use speculoos::prelude::*;
+
+peg::parser! {
+    grammar expression_parser for str {
+
+    }
+}
 
 fn main() {
     let mut logger = StandardLogger::new();
@@ -51,10 +59,17 @@ fn main() {
             Using max depth of 0 will apply the expression 
             for files only in the current directory, and will not search subdirectories")
         )
+        //expression
         .arg(Arg::new("name")
             .long("name")
             .action(ArgAction::Set)
             .help("Base of file name (the path with the leading directories removed) matches shell pattern")
+        )
+        .arg(Arg::new("type")
+            .value_parser(value_parser!(String))
+            .long("type")
+            .action(ArgAction::Set)
+            .help("file name, provided as a character. Use multiple characters to signify multiple types")
         ).get_matches();
 
     // parse the cmd arguments
@@ -88,17 +103,26 @@ fn main() {
         _ => "."
     };
 
+    let types: Vec<Test> = Vec::new();
+
     let name: &str = match matches.get_one::<String>("name") {
         Some(x) => x,
         _ => "*"
     };
+    types.push(Test::Name(name.to_string()));
+
+    let provided_file_type: &str = match matches.get_one::<String>("type") {
+        Some(x) => x,
+        _ => ""
+    };
+    types.push(Test::Types(provided_file_type.to_string()));
 
     // todo log as verbose:
     // println!("Starting_path: {}", starting_path);
     // println!("Name: {}", name);
 
     let searcher = Searcher::new(max_depth.copied(), symlink_setting);
-    searcher.search_directory_path(Path::new(starting_path), name, &mut logger, None);
+    searcher.search_directory_path(Path::new(starting_path), types, &mut logger, None);
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -107,12 +131,6 @@ enum SymLinkSetting {
     Follow,
     OnlyCommandLineArgs
 }
-
-// impl ValueEnum for SymLinkSetting {
-    // fn value_variants<'a>() -> &'a [Self] {
-    // }
-// }
-
 
 #[derive(Clone, Debug, PartialEq)]
 enum LogLine {
@@ -142,6 +160,11 @@ impl Logger for StandardLogger {
     }
 }
 
+enum Test {
+    Name(String),
+    Types(String)
+}
+
 struct Searcher {
     max_depth: Option<u32>,
     symlink_setting: SymLinkSetting
@@ -155,7 +178,7 @@ impl Searcher {
         }
     }
 
-    pub fn search_directory_path<T: Logger>(&self, directory_path: &Path, name: &str, logger: &mut T, current_depth: Option<u32>) {
+    pub fn search_directory_path<T: Logger>(&self, directory_path: &Path, test: &Test, logger: &mut T, current_depth: Option<u32>) {
         let current_depth = current_depth.unwrap_or(0);
         let read_dir = match fs::read_dir(directory_path) {
             Ok(res) => {
@@ -189,23 +212,39 @@ impl Searcher {
                     }
                     Err(error) if error.kind() == io::ErrorKind::NotFound => {
                         logger.log(LogLine::StdErr(format!("Broken symlink: {}", ele.path().to_str().unwrap().to_string())));
-                        continue;
                     }
                     Err(_) => {
                         unreachable!("We have handled both cases where read_link would result in an error, so this should be unreachable");
                     }
                 }
             }
-            if file_name == name {
-                logger.log(LogLine::StdOut(directory_path.join(name).to_str().unwrap().to_string()));
+    
+            // todo make operator logic, by default all tests have to pass to return a find.
+            let line_to_log;
+            line_to_log = match test {
+                Test::Name(name) if file_name.to_str().unwrap() == name => true,
+                Test::Types(provided_file_type) if 
+                (file_type.is_block_device() && provided_file_type.contains('b')) &&
+                (file_type.is_char_device() && provided_file_type.contains('c')) &&
+                (file_type.is_dir() && provided_file_type.contains('d')) &&
+                (file_type.is_file() && provided_file_type.contains('f')) &&
+                (file_type.is_fifo() && provided_file_type.contains('p')) &&
+                (file_type.is_symlink() && provided_file_type.contains('l') && self.symlink_setting != SymLinkSetting::Follow) &&
+                (file_type.is_socket() && provided_file_type.contains('s')) => true,
+                _ => false,
+            };
+            if line_to_log {
+                logger.log(LogLine::StdOut(directory_path.join(file_name).to_str().unwrap().to_string()));
                 continue;
             }
+// logger.log(LogLine::StdOut(directory_path.join(file_name).to_str().unwrap().to_string()));
+
             if file_type.is_dir() && (self.max_depth.is_some() && current_depth < self.max_depth.unwrap() || self.max_depth.is_none()) {
                 let file_name = ele.file_name();
                 let file_name: &str = file_name.to_str().unwrap();
                 let directory_path = directory_path.join(file_name);
                 let directory_path = directory_path.as_path();
-                self.search_directory_path(directory_path, name, logger, Some(current_depth + 1));
+                self.search_directory_path(directory_path, test, logger, Some(current_depth + 1));
             }
         }
     }
